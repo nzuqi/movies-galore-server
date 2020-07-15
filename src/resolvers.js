6a59e95c45a81af672a114ecd44914a69d12fac8
Martin Nzuki <iamnzuki@gmail.com>
2020-07-15 16:33:09 +0300

    Fetch show by id

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,7 @@
 module.exports = {
     Query: {
+        show: async (_, { id }, { dataSources }) =>
+            dataSources.showsAPI.getShowById({ id: id }),
         shows: async (_, __, { dataSources }) =>
             dataSources.showsAPI.getAllShows(),
     },
